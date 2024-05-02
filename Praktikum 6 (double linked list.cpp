c1ccba68697c7a3dbac8899a6b7d953a63d02dff
gibranrafii <gibranrafi21@gmail.com>
2024-05-02 11:46:40 +0700

    Menyelesaikan prosedur addNote

--- Praktikum 6 (double linked list.cpp
+++ Praktikum 6 (double linked list.cpp
@@ -34,6 +34,31 @@ void addNote() {
         newNode->prev = NULL;       //STEP 5: make the new node point to NULL
         START = newNode;            //STEP 6: make the new node the first node
     }
+    else {
+        // insert the new node in the middle or at the end
+        Node* current = START; // STEP 1.a: start from the first
+        Node* previous = NULL; // STEP 1.b: previous node is NULL initially
+
+        while (current != NULL && current->noMhs < newNode->noMhs) { // STEP 1.c: traver
+            previous = current; // STEP 1.d: move the previous to the current Node
+            current = current->next; // STEP 1.e: move the current to the next node
+        }
+
+        newNode->next = current; // STEP 4: Make the next field of the new node point to current
+        newNode->prev = previous; // STEP 5: Make the previous field of the new node point to previous
+        
+        if (current != NULL) {
+            current->prev = newNode; // STEP 6: Make the previous field of the current
+        }
+
+        if (previous != NULL) {
+            previous->next = newNode; // STEP 7 : make the next field of the previous node
+        }
+        else {
+            //If previous is still NULL, it means newNode is now the first node
+            START = newNode;
+        }
+    }
 }
 
 int main()
