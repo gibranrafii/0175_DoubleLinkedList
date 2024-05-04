dddd584e944242d1bdafb8840bb6b21fa9b1b6eb
gibranrafii <gibranrafi21@gmail.com>
2024-05-04 15:18:14 +0700

    Membuat prosedure delete Node

--- Praktikum 6 (double linked list.cpp
+++ Praktikum 6 (double linked list.cpp
@@ -73,6 +73,15 @@ bool search(int rollNo, Node** previous, Node** current)
     return (*current != NULL);
 }
 
+void deleteNode()
+{
+    Node* previous, * current;
+    int rollNo;
+
+    cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+    cin >> rollNo;                                                                  //STEP 3: get the roll number to be deleted
+
+
 int main()
 {
     
