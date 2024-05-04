66059149d230caf695f5d67071a85e1df4162185
gibranrafii <gibranrafi21@gmail.com>
2024-05-04 15:19:36 +0700

    Membuat kondisi if current == null

--- Praktikum 6 (double linked list.cpp
+++ Praktikum 6 (double linked list.cpp
@@ -97,7 +97,14 @@ void deleteNode()
         current = current->next;
     }
 
+    if (current == NULL)
+    {
+        cout << "\033[31mThe record with roll number " << rollNo << "not found\033[0m" << endl;
+        return;
+    }
+
     
+
 
 int main()
 {
